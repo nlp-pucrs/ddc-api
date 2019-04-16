61ef97cb66c8ae78b5da028c304c68d47fd8a3e3
Marcelo Arocha <marceloarocha@gmail.com>
2019-04-15 21:21:35 -0300

    add java client example

--- examples/example.java
+++ examples/example.java
@@ -1,54 +1,76 @@
 // Ubuntu instalation
 // sudo apt install default-jdk 
-// Tutorial: https://www.techcoil.com/blog/how-to-upload-a-file-via-a-http-multipart-request-in-java-without-using-any-external-libraries/
-// WARNING: Example still not working
+// You can make this code simpler if you use a lib like HttpClient (https://hc.apache.org/httpcomponents-client-ga/index.html)
+// examples at: https://www.baeldung.com/httpclient-post-http-request (POST Multipart Request)
 
-import java.io.DataOutputStream;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-public class example {
+public class Example {
 
-    public static void main(String[] args) throws Exception {
+	private static final String CRLF = "\r\n";
+	private static final String CHARSET = StandardCharsets.UTF_8.name();
 
-		URL url = new URL("http://127.0.0.1:5000/score");
-		HttpURLConnection con = (HttpURLConnection) url.openConnection();
-		con.setRequestMethod("POST");
-		con.addRequestProperty("Content-Type", "multipart/form-data;");
-        con.setDoOutput(true);
-        con.setDoInput(true);
+	public static void main(String[] args) throws Exception {
+		String url = "http://127.0.0.1:5000/score";
+		String uploadFilePath = "../data/test.csv.gz";
+		String outputFilePath = "output.csv.gz";
 
-		con.connect();
+		File binaryFile = new File(uploadFilePath);
+		String boundary = Long.toHexString(System.currentTimeMillis());
 
-		OutputStream output = con.getOutputStream();
-		DataOutputStream writer = new DataOutputStream(output);
-		String post = "userid=hospital";
-		writer.writeBytes(post);
+		URLConnection connection = new URL(url).openConnection();
+		connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+		connection.setDoOutput(true);
 
-        File gzippedData = new File("../data/test.csv.gz");
-        FileInputStream input = new FileInputStream(gzippedData);
+		try (
+			OutputStream output = connection.getOutputStream();
+			PrintWriter writer = new PrintWriter(new OutputStreamWriter(output, CHARSET), true);
+		) {
+			// add userid
+			addString(writer, boundary, "userid", "hospital");
 
-		int bytesRead;
-		byte[] dataBuffer = new byte[1024];
-		while((bytesRead = input.read(dataBuffer)) != -1) {
-		    output.write(dataBuffer, 0, bytesRead);
+			// add file
+			addFile(writer, output, boundary, "file", binaryFile);
+
+			writer.append("--" + boundary + "--").append(CRLF).flush();
 		}
+
+		Files.copy(((HttpURLConnection) connection).getInputStream(), Paths.get(outputFilePath));
+		System.out.println("Response code: " + ((HttpURLConnection) connection).getResponseCode());
+		System.out.println("Response content: " + outputFilePath);
+	}
+
+	private static void addString(PrintWriter writer, String boundary, String paramKey, String paramValue) {
+		writer.append("--" + boundary).append(CRLF);
+		writer.append("Content-Disposition: form-data; name=\"" + paramKey + "\"").append(CRLF);
+		writer.append("Content-Type: text/plain; charset=" + CHARSET).append(CRLF);
+		writer.append(CRLF).append(paramValue).append(CRLF).flush();
+	}
+
+	private static void addFile(PrintWriter writer, OutputStream output, String boundary, String paramKey,
+			File binaryFile) throws IOException {
+		writer.append("--" + boundary).append(CRLF);
+		writer.append(
+			"Content-Disposition: form-data; name=\"" + paramKey + "\"; filename=\"" + binaryFile.getName() + "\""
+		).append(CRLF);
+		writer.append("Content-Type: " + URLConnection.guessContentTypeFromName(binaryFile.getName())).append(CRLF);
+		writer.append("Content-Transfer-Encoding: binary").append(CRLF);
+		writer.append(CRLF).flush();
+
+		Files.copy(binaryFile.toPath(), output);
 		output.flush();
 
-		System.out.println(con.getResponseCode());
-		BufferedReader resultado = new BufferedReader(
-                new InputStreamReader(con.getInputStream()));
-
-		con.disconnect();
-
-    }
-
+		writer.append(CRLF).flush();
+	}
+	
 }
-
