170fb6083222ad6c8fd27a72758c55ff7e98eff7
Henrique Dias <heukirne@gmail.com>
2019-12-21 15:30:06 -0300

    fiz test

--- tests/test_api.py
+++ tests/test_api.py
@@ -19,26 +19,26 @@ def test_empty_post(client):
 	assert response.status_code == 400
 
 def test_wrong_user(client):
-	response = client.post('/score', data={'userid':'hospital10'})
+	response = client.post('/score', data={'userid':'5'})
 	assert response.status_code == 401
 
 def test_empty_file(client):
-	response = client.post('/score', data={'userid':'hospital', 'file':''})
+	response = client.post('/score', data={'userid':'1', 'file':''})
 	assert response.status_code == 406
 
 def test_not_gziped_file(client):
-	file = {'file': open('./data/tokens.csv', 'rb')}
-	response = client.post('/score', data={'userid':'hospital', 'file': file})
+	file = {'file': open('./data/test.csv', 'rb')}
+	response = client.post('/score', data={'userid':'1', 'file': file})
 	assert response.status_code == 412
 
 def test_wrong_header_file(client):
 	file = {'file': open('./data/medications.csv.gz', 'rb')}
-	response = client.post('/score', data={'userid':'hospital', 'file': file})
+	response = client.post('/score', data={'userid':'1', 'file': file})
 	assert response.status_code == 417
 
 def test_correct_file(client):
 	file = {'file': open('./data/test.csv.gz', 'rb')}
-	response = client.post('/score', data={'userid':'hospital', 'file': file})
+	response = client.post('/score', data={'userid':'1', 'file': file})
 
 	assert response.status_code == 200
 
